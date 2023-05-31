909bfbf7ab6a525d86d78de9dac980797d444e71
Melanie Rodas <mrodasl@uni.pe>
2023-05-30 20:14:57 -0500

    added a function to orderByYear

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 import { coldplayVideos } from "../data/data.js";
-console.log(coldplayVideos);
 
 function createCard(element) {
-  console.log(element);
   const card = `<div
   class="card"
 >
@@ -34,7 +32,36 @@ function createCard(element) {
   return card;
 }
 
+function orderByYear(direction) {
+  let showCards = document.getElementById("showCards");
+
+  //eliminamos el contenido
+  showCards.innerHTML = "";
+
+  let orderedArrayVideos = copiedColdplayVideos.sort((a, b) => {
+    console.log(a);
+    console.log(b);
+    if (direction === "asc") {
+      return a.year > b.year ? 1 : -1;
+    } else {
+      return a.year < b.year ? 1 : -1;
+    }
+  });
+  console.log(orderedArrayVideos);
+  //pintamos el resultado
+  orderedArrayVideos.map((video) => (showCards.innerHTML += createCard(video)));
+}
+
 const showCards = document.getElementById("showCards");
+let copiedColdplayVideos = [...coldplayVideos];
+const buttonAscByYear = document.getElementById("ascByYear");
+const buttonDescByYear = document.getElementById("descByYear");
+buttonAscByYear.addEventListener("click", function () {
+  orderByYear("asc");
+});
+buttonDescByYear.addEventListener("click", function () {
+  orderByYear("desc");
+});
 
 for (let i = 0; i < coldplayVideos.length; i++) {
   showCards.innerHTML += createCard(coldplayVideos[i]);
